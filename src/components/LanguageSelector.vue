079b141597a48527bd84340fd3fbefd90717fee8
Manuel Abascal <manuelsuarezabascal@gmail.com>
2019-05-27 04:25:46 -0400

    Created functionality to swap language options & include font awesome CDN

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="language-selector">
     <b-row class="justify-content-center">
-      <span class="mx-2 my-4">
+      <span class="mr-2 my-4">
         <strong>From:</strong>
       </span>
-      <b-col class="my-3 p-0" col lg="3" md="4" sm="10" cols="10">
+      <b-col class="my-3 p-0" col lg="3" md="3" sm="10" cols="10">
         <!-- When input changes it calls related method to emit selected data. -->
         <b-form-select v-model="optionFrom" @input="onOptionFromSelect">
             <option :value="null">Select an language</option>
             <option v-for="option in languageOptions" :key="option.value" :value="option.value">{{option.text}}</option>
         </b-form-select>
       </b-col>
-      <br>
+
       <!-- Interchanges Language Options -->
-      <span @click="interchangeLanguages">Interchange languages</span>
-      
+      <b-col col lg="1" md="2" sm="10" cols="12">
+        <i  @click="interchangeLanguages" class="fa fa-exchange mt-3 ml-4 interchange-lang-btn" aria-hidden="true"></i>
+      </b-col>
+
       <span class="mr-2 ml-4 my-4">
         <strong>To:</strong>
       </span>
-      <b-col class="my-3 p-0" col lg="3" md="4" sm="10" cols="10">
+      <b-col class="my-3 p-0" col lg="3" md="3" sm="10" cols="10">
         <!-- When input changes it calls related method to emit selected data. -->
         <b-form-select v-model="optionTo" @input="onOptionToSelect">
             <option :value="null">Select an language</option>
@@ -60,4 +62,17 @@ export default {
 };
 </script>
 <style scoped>
+
+.interchange-lang-btn {
+  background-color: rgb(216, 216, 216);
+  border-radius: 8px;
+  cursor: pointer;
+  padding: 10px 15px;
+}
+  @media only screen and (max-width: 575px) {
+  .interchange-lang-btn {
+    margin-top: 0 !important;
+  }
+}
+
 </style>
